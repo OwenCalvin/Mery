11487efd32b57b4d8f710476ee0a4781a8140ddc
Owen Gombas <owen.gombas@gmail.com>
2018-03-20 12:10:39 +0100

    See

---
+++ src/renderer/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app" @mousemove="setPosition($event)" @mouseout="unHover()" :style="{background: getBackground}">
+    <router-view class="hover-hidden"></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'see',
+    data: function () {
+      return {
+        posX: 0,
+        posY: 0
+      }
+    },
+    computed: {
+      getBackground: function () {
+        return `radial-gradient(circle at ${this.posX}px ${this.posY}px, transparent 200px, rgba(255, 255, 255, .5) 0%)`
+      }
+    },
+    methods: {
+      setPosition: function (ev) {
+        this.posX = ev.pageX
+        this.posY = ev.pageY
+      },
+      unHover: function () {
+        console.log('test')
+      }
+    }
+  }
+</script>
+
+<style>
+  body {
+    padding: 0;
+    margin: 0;
+  }
+  #app {
+    position: absolute;
+    /*background: radial-gradient(circle at 100px 30px, transparent 20%, white 20%);*/
+    background-size: 100% 100%;
+    min-height: 100%;
+    width: 100%;
+  }
+</style>
---
+++ src/renderer/components/MainPage.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="wrapper">
+    
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'main-page',
+    components: {},
+    methods: {}
+  }
+</script>
+
+<style>
+  #wrapper {
+    width: 100%;
+    height: 50px;
+  }
+</style>
